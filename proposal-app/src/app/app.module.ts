29070a57bef14c1aa59316fa1dcd141c06439fba
Nick DeRobertis <whoopnip@gmail.com>
2018-12-24 13:42:51 -0500

    Hook up custom material module to app

--- proposal-app/src/app/app.module.ts
+++ proposal-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomMaterialModuleModule } from './custom-material-module/custom-material-module.module'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CustomMaterialModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
