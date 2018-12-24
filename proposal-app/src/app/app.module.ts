0ac816083bfd6c2b808e277c483e5ea95b40537c
Nick DeRobertis <whoopnip@gmail.com>
2018-12-24 15:51:42 -0500

    Add landing page component and move main content there

--- proposal-app/src/app/app.module.ts
+++ proposal-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CustomMaterialModuleModule } from './custom-material-module/custom-material-module.module'
+import { CustomMaterialModuleModule } from './custom-material-module/custom-material-module.module';
+import { LandingPageComponent } from './landing-page/landing-page.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
