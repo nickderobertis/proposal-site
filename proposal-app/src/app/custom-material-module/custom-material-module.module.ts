d4256a6c890f94c137973080e0eb1f4bbd2589d5
Nick DeRobertis <whoopnip@gmail.com>
2018-12-24 13:45:09 -0500

    Add material card and grid components

--- proposal-app/src/app/custom-material-module/custom-material-module.module.ts
+++ proposal-app/src/app/custom-material-module/custom-material-module.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    MatGridListModule,
+    MatCardModule
+  ],
+  exports: [
+    MatGridListModule,
+    MatCardModule
   ]
 })
 export class CustomMaterialModuleModule { }
