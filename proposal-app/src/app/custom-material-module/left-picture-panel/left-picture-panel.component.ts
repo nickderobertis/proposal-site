5a4c97a6807a7cb3e934147525e24caf2d1ff383
Nick DeRobertis <whoopnip@gmail.com>
2018-12-27 02:17:00 -0500

    Add option for positioning of picture in picture panel

--- proposal-app/src/app/custom-material-module/left-picture-panel/left-picture-panel.component.ts
+++ proposal-app/src/app/custom-material-module/left-picture-panel/left-picture-panel.component.ts
@@ -10,10 +10,53 @@ export class LeftPicturePanelComponent implements OnInit {
   @Input() imageAlt: string = 'An image';
   @Input() imageCaption: string = '';
   @Input() backgroundColor: string = 'none';
+  @Input() picturePosition: string = 'right';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get shouldDisplayLeftPicture(): boolean {
+    return this.picturePosition === 'left';
+  }
+
+  get shouldDisplayInputArea(): boolean {
+    return this.picturePosition != 'center';
+  }
+
+  get shouldDisplayRightPicture(): boolean {
+    return this.picturePosition != 'left';
+  }
+
+  get shouldDisplayPadding(): boolean {
+    return this.picturePosition == 'center';
+  }
+
+  get numCols(): number {
+    if (!this.shouldDisplayInputArea) {
+      // Centered, no text area, make picture bigger
+      return 5;
+    }
+    else {
+      // Two thirds for text area, one third for picture
+      return 3;
+    }
+  }
+
+  get numPictureCols(): number {
+    if (!this.shouldDisplayInputArea) {
+      // Centered, no text area, make picture bigger
+      return 3;
+    }
+    else {
+      // Two thirds for text area, one third for picture
+      return 1;
+    }
+  }
+
+  get numTextCols(): number {
+    return this.numCols - this.numPictureCols
   }
 
 }
