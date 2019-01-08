a370f9fb8e292b0b8a77f154fcea499f9cbcc168
Nick DeRobertis <whoopnip@gmail.com>
2019-01-08 11:22:39 -0500

    Create deployment script. Working for github url.

---
+++ proposal-app/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BASE_HREF="https://whoopnip.github.io/proposal-site/";
+GH_TOKEN="0385570948a8ac00ddd02ebb9f2275a237828d03";
+PROJECT_DIR="dist/proposal-app";
+COMMIT_MESSAGE="Deployment commit at $(date)";
+REPO_HREF="https://$GH_TOKEN@github.com/whoopnip/proposal-site.git";
+NAME_OF_USER="Nick DeRobertis";
+EMAIL_OF_USER="whoopnip@gmail.com";
+
+echo "Building application with Angular CLI for URL $BASE_HREF";
+ng build --prod --base-href $BASE_HREF;
+
+npx angular-cli-ghpages \
+  --dir=$PROJECT_DIR \
+  --message "$COMMIT_MESSAGE" \
+  --repo "$REPO_HREF" \
+  --name "$NAME_OF_USER" \
+  --email "$EMAIL_OF_USER" \
+  --no-silent;
